8791661c91015024a66051154d7a4f6ac8ca7f62
Ariel Farias <eu.ariel.farias@gmail.com>
2019-03-18 12:25:22 -0300

    Test while true

--- run.sh
+++ run.sh
@@ -2,15 +2,20 @@
 # Adapted from Alex Kleissner's post, Running a Phoenix 1.3 project with docker-compose
 # https://medium.com/@hex337/running-a-phoenix-1-3-project-with-docker-compose-d82ab55e43cf
 
-set -e
+# set -e
 
-# Ensure the app's dependencies are installed
-mix deps.get
+# # Ensure the app's dependencies are installed
+# mix deps.get
 
-echo "\nTesting the installation..."
-# "Proove" that install was successful by running the tests
-mix test
+# echo "\nTesting the installation..."
+# # "Proove" that install was successful by running the tests
+# mix test
 
-echo "\n Launching Phoenix web server..."
-# Start the phoenix web server
-mix phx.server
+# echo "\n Launching Phoenix web server..."
+# # Start the phoenix web server
+# mix phx.server
+
+while : ; do
+  echo "waiting"
+  sleep 60
+done
