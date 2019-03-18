5045402aec4622bc38d0b62afe010e7dc821151d
Ariel Farias <eu.ariel.farias@gmail.com>
2019-03-18 11:57:36 -0300

    Revert changes to the first try

--- Dockerfile
+++ Dockerfile
@@ -3,18 +3,15 @@
 FROM elixir:1.8.1-alpine
 
 # Install debian packages
-RUN apk --update add openssh
+RUN apk --update add openssh-client
 RUN apk add build-base inotify-tools
 
 ADD . /app
-COPY . /app
-WORKDIR /app
 
 # Install Phoenix packages
 RUN mix local.hex --force
 RUN mix local.rebar --force
 RUN mix archive.install --force hex phx_new 1.4.2
 
+WORKDIR /app
 EXPOSE 4000
-
-CMD ["/app/run.sh"]
--- run.sh
+++ run.sh
@@ -2,25 +2,15 @@
 # Adapted from Alex Kleissner's post, Running a Phoenix 1.3 project with docker-compose
 # https://medium.com/@hex337/running-a-phoenix-1-3-project-with-docker-compose-d82ab55e43cf
 
-# set -e
+set -e
 
-# # Ensure the app's dependencies are installed
-# exec mix deps.get
+# Ensure the app's dependencies are installed
+mix deps.get
 
-# echo "\nTesting the installation..."
-# # "Proove" that install was successful by running the tests
-# exec mix test
+echo "\nTesting the installation..."
+# "Proove" that install was successful by running the tests
+mix test
 
-# echo "\n Launching Phoenix web server..."
-# # Start the phoenix web server
-# exec mix phx.server
-
-
-# Start sshd if we don't use the init system
-/usr/sbin/sshd -p 22 &
-
-echo "This is where your application would start..."
-while : ; do
-  echo "waiting"
-  sleep 60
-done
+echo "\n Launching Phoenix web server..."
+# Start the phoenix web server
+mix phx.server
